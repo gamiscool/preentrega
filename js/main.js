c34e81ac2f1de99656cdc6827c8693dc773795ab
Agustin Muñiz <96355449+gamiscool@users.noreply.github.com>
2024-05-01 17:58:36 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+//? Prueba de Pre Entrega
+
+//? Este proyecto de pre-entrega sera un proyecto enfocado en una agencia de marketing que propone los servicios/planes de acuerdo a las exigencias del cliente.
+
+
+// let nombre = `agus`;
+// let apellido = `muniz`;
+// let nombreCompleto = nombre + " " + apellido;
+// let edad = 27;
+
+// console.log(nombreCompleto, String(edad));`
+
+// let saludo = "Hola";
+// let nombre = prompt("Ingresá tu nombre");
+// let saludoCompleto = saludo + " " + nombre;
+
+// console.log(saludoCompleto);
+
+let nombre = prompt("Cómo es tu nombre");
+let saludo = `Hola,` + " " + nombre + " " + `Bienvenido a Gam.ar, el mejor marketing para tu negocio`;
+
+let saludoCompleto = saludo;
+
+let anioUsuario = prompt(`En qué año naciste?`);
+
+
+const anioActual = 2024;
+let edadUsuario = anioActual - anioUsuario;
+
+console.log(saludoCompleto, + " " + String("Tenés") + " " + String((edadUsuario)) + " años no?");
+
+
+
+
