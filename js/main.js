1e129e6cd4d367b6a70788a5e7bb4377fd6da8ca
Agustin Muñiz <96355449+gamiscool@users.noreply.github.com>
2024-05-05 16:51:09 -0300

    main.js

--- js/main.js
+++ js/main.js
@@ -24,32 +24,32 @@ const anioActual = 2024;
 let edadUsuario = anioActual - anioUsuario;
 alert(`Tu nombre es ${nombre} y tienes ${edadUsuario} años.`);
 
-let comercioOk = prompt("¿Tienes un comercio/negocio? (Responde 'SI' o 'NO')");
+let comercioOk = prompt("¿Tenés un comercio/negocio? (Responde 'SI' o 'NO')");
 
 if (comercioOk.toUpperCase() === 'SI') {
-    let seleccionPlan = prompt("Excelente. ¿Qué plan de marketing te interesa?\n1. Plan Básico ($1000)\n2. Plan Avanzado ($2500)\n3. Plan Premium ($3000)\nEscribe el número del plan:");
+    let seleccionPlan = prompt("Perfecto. ¿Qué plan de marketing te interesa para tu negocio?\n1. Plan Básico ($1000)\n2. Plan Avanzado ($2500)\n3. Plan Premium ($3000)\nEscribe el número del plan:");
     
     switch (seleccionPlan) {
         case '1':
             console.log("Has seleccionado el Plan Básico. El valor es de $1000.");
-            alert("Has seleccionado el Plan Básico. El valor es de $1000.");
+            alert("Seleccionaste el Plan Básico. El valor es de $1000.");
             break;
         case '2':
-            console.log("Has seleccionado el Plan Avanzado. El valor es de $2500.");
-            alert("Has seleccionado el Plan Avanzado. El valor es de $2500.");
+            console.log("Seleccionaste el Plan Avanzado. El valor es de $2500.");
+            alert("Seleccionaste el Plan Avanzado. El valor es de $2500.");
             break;
         case '3':
-            console.log("Has seleccionado el Plan Premium. El valor es de $3000.");
+            console.log("Seleccionaste el Plan Premium. El valor es de $3000.");
             alert("Has seleccionado el Plan Premium. El valor es de $3000.");
             break;
         default:
-            console.log("No has seleccionado un plan válido.");
-            alert("No has seleccionado un plan válido.");
+            console.log("No has seleccionado una opción válida.");
+            alert("Seleccionaste el una opción válida.");
             break;
     }
 } else if (comercioOk.toUpperCase() === 'NO') {
-    console.log("Lo siento, parece que no podemos ayudarte si no tienes un negocio.");
-    alert("Lo siento, parece que no podemos ayudarte si no tienes un negocio.");
+    console.log("Dale Vago, ponete un kiosko y volvé... (raton)");
+    alert("Dale Vago, ponete un kiosko y volvé... (raton)");
 } else {
     console.log("No entendí tu respuesta. Por favor, responde 'SI' o 'NO'.");
     alert("No entendí tu respuesta. Por favor, responde 'SI' o 'NO'.");
